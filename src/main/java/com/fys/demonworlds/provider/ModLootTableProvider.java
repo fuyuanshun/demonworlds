7b4ec687d66d816d88a62b6d21e5452f3eb96538
fuyuanshun521@gmail.com <fuyuanshun521@gmail.com>
2025-10-23 09:15:07 +0800

    战利品列表调整，熔炉数据生成

--- src/main/java/com/fys/demonworlds/provider/ModLootTableProvider.java
+++ src/main/java/com/fys/demonworlds/provider/ModLootTableProvider.java
@@ -3,6 +3,7 @@ package com.fys.demonworlds.provider;
 import com.fys.demonworlds.block.ModBlocks;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.data.loot.LootTableProvider;
@@ -10,8 +11,16 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
 import java.util.List;
@@ -34,6 +43,27 @@ public class ModLootTableProvider extends BlockLootSubProvider {
     protected void generate() {
         dropSelf(ModBlocks.SUN_BLOCK.get());
 
+    }
+
+    /**
+     * 自定义掉落物：指定掉落物、掉落最小数量、掉落最大数量
+     * @param block 指定方块，战利品就是该方块的掉落物
+     * @param item 掉落物
+     * @param min 掉落最小数量
+     * @param max 掉落最大数量
+     * @return
+     */
+    protected LootTable.Builder createCopperOreLikeDrops(Block block, Item item, float min, float max) {
+        HolderLookup.RegistryLookup<Enchantment> registrylookup = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
+        return this.createSilkTouchDispatchTable(
+                block,
+                (LootPoolEntryContainer.Builder<?>)this.applyExplosionDecay(
+                        block,
+                        LootItem.lootTableItem(item)
+                                .apply(SetItemCountFunction.setCount(UniformGenerator.between(min, max)))
+                                .apply(ApplyBonusCount.addOreBonusCount(registrylookup.getOrThrow(Enchantments.FORTUNE)))
+                )
+        );
     }
 
     @Override
--- src/main/java/com/fys/demonworlds/provider/ModRecipeProvider.java
+++ src/main/java/com/fys/demonworlds/provider/ModRecipeProvider.java
@@ -1,15 +1,16 @@
 package com.fys.demonworlds.provider;
 
 import com.fys.demonworlds.block.ModBlocks;
+import com.fys.demonworlds.constants.ModConstants;
 import com.fys.demonworlds.item.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.RecipeCategory;
-import net.minecraft.data.recipes.RecipeOutput;
-import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.level.ItemLike;
 import net.neoforged.neoforge.common.conditions.IConditionBuilder;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -19,12 +20,18 @@ import java.util.concurrent.CompletableFuture;
  */
 public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
 
+    public static final List<ItemLike> SUN_BLOCK_ORE = List.of(ModBlocks.SUN_BLOCK);
+
     public ModRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries);
     }
 
     @Override
     protected void buildRecipes(RecipeOutput recipeOutput) {
+        //熔炉配方
+        oreSmelting(recipeOutput, SUN_BLOCK_ORE, RecipeCategory.MISC, ModBlocks.SUN_BLOCK, 0.8F, 200, "sun_block");
+
+        //工作台配方
         ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, ModItems.DEMON_FRUIT_SUN)
                 .pattern("###")
                 .pattern("###")
@@ -32,5 +39,59 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .define('#', ModBlocks.SUN_BLOCK)
                 .unlockedBy(getHasName(ModBlocks.SUN_BLOCK), has(ModBlocks.SUN_BLOCK))
                 .save(recipeOutput);
+    }
+
+    protected static void oreSmelting(
+            RecipeOutput recipeOutput, List<ItemLike> ingredients, RecipeCategory category, ItemLike result, float experience, int cookingTime, String group
+    ) {
+        oreCooking(
+                recipeOutput,
+                RecipeSerializer.SMELTING_RECIPE,
+                SmeltingRecipe::new,
+                ingredients,
+                category,
+                result,
+                experience,
+                cookingTime,
+                group,
+                "_from_smelting"
+        );
+    }
+
+    protected static void oreBlasting(
+            RecipeOutput recipeOutput, List<ItemLike> ingredients, RecipeCategory category, ItemLike result, float experience, int cookingTime, String group
+    ) {
+        oreCooking(
+                recipeOutput,
+                RecipeSerializer.BLASTING_RECIPE,
+                BlastingRecipe::new,
+                ingredients,
+                category,
+                result,
+                experience,
+                cookingTime,
+                group,
+                "_from_blasting"
+        );
+    }
+
+    protected static <T extends AbstractCookingRecipe> void oreCooking(
+            RecipeOutput recipeOutput,
+            RecipeSerializer<T> serializer,
+            AbstractCookingRecipe.Factory<T> recipeFactory,
+            List<ItemLike> ingredients,
+            RecipeCategory category,
+            ItemLike result,
+            float experience,
+            int cookingTime,
+            String group,
+            String suffix
+    ) {
+        for (ItemLike itemlike : ingredients) {
+            SimpleCookingRecipeBuilder.generic(Ingredient.of(itemlike), category, result, experience, cookingTime, serializer, recipeFactory)
+                    .group(group)
+                    .unlockedBy(getHasName(itemlike), has(itemlike))
+                    .save(recipeOutput, ModConstants.MOD_ID+":"+getItemName(result) + suffix + "_" + getItemName(itemlike));
+        }
     }
 }
