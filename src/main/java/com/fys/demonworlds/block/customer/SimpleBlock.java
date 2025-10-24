fb661346b6a9b97e64c315cac04fe63b38f88363
fuyuanshun521@gmail.com <fuyuanshun521@gmail.com>
2025-10-24 09:24:14 +0800

    碰撞箱调整

--- src/main/java/com/fys/demonworlds/block/customer/SimpleBlock.java
+++ src/main/java/com/fys/demonworlds/block/customer/SimpleBlock.java
@@ -1,9 +1,11 @@
 package com.fys.demonworlds.block.customer;
 
+import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -17,11 +19,13 @@ import java.util.stream.Stream;
  */
 public class SimpleBlock extends Block {
 
+    public static final MapCodec<SimpleBlock> CODEC = simpleCodec(SimpleBlock::new);
+
     private static final VoxelShape SHAPE_BY_AGE = Stream.of(
-            Shapes.box(0, 2, 0, 16, 14, 16),
-            Shapes.box(0, 1, -1, 16, 2, 16),
-            Shapes.box(0, 14, -1, 16, 15, 16)
-    ).reduce(Shapes::or).get();
+            Block.box(1, 2, 1, 16, 14, 16),
+            Block.box(0, 0, 1, 16, 2, 16),
+            Block.box(0, 14, 1, 16, 16, 16)
+    ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get();
 
     public SimpleBlock(Properties properties) {
         super(properties);
@@ -30,5 +34,10 @@ public class SimpleBlock extends Block {
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return SHAPE_BY_AGE;
+    }
+
+    @Override
+    protected MapCodec<? extends Block> codec() {
+        return CODEC;
     }
 }
